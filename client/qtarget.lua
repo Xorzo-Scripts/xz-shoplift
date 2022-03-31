e0d72e1e3c97455c817c519b1adc081941cd0fe0
Benzo <102178921+Benzo00@users.noreply.github.com>
2022-03-31 16:47:33 +0200

    added more shelves for qtarget

--- client/qtarget.lua
+++ client/qtarget.lua
@@ -1,4 +1,4 @@
-exports.qtarget:AddTargetModel({"v_ret_247shelves05"}, {
+exports.qtarget:AddTargetModel({"v_ret_247shelves05", "v_ret_247shelves04", "v_ret_247shelves03"}, {
 options = {
 {
 event = "Steal",
