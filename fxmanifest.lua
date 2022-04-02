9c090267f9c8d26c380fa5cad3cfd83375511119
Benzo <102178921+Benzo00@users.noreply.github.com>
2022-04-02 16:08:13 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,11 +4,7 @@ games { 'rdr3', 'gta5' }
 author 'Benzo#6565, xz-scripts'
 description 'XZ-Shoplift'
 
-shared_scripts  {
-	'@es_extended/imports.lua',
-	'shared/config.lua',
-}
-
+shared_scripts  {'@es_extended/imports.lua', 'shared/config.lua',}
 client_scripts {'client/*.lua'}
 server_scripts {'server/*.lua'}
 
