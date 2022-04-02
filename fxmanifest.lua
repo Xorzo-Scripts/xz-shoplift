cf27c206f854085b551e39b0dd6eaa6b030f5f90
Benzo <102178921+Benzo00@users.noreply.github.com>
2022-04-02 16:07:36 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,18 +2,13 @@ fx_version 'adamant'
 games { 'rdr3', 'gta5' }
 
 author 'Benzo#6565, xz-scripts'
-
+description 'XZ-Shoplift'
 
 shared_scripts  {
 	'@es_extended/imports.lua',
 	'shared/config.lua',
 }
 
-client_scripts { 
-	'client/client.lua',
-	'client/qtarget.lua',
-}
-server_scripts {
-  'server/server.lua',
-}
+client_scripts {'client/*.lua'}
+server_scripts {'server/*.lua'}
 
