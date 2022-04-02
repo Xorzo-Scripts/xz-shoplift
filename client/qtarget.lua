1c65df8ea4fee92492c2420bad1fdf478bf7527f
Benzo <102178921+Benzo00@users.noreply.github.com>
2022-04-02 12:33:52 +0200

    Update qtarget.lua

--- client/qtarget.lua
+++ client/qtarget.lua
@@ -1,7 +1,7 @@
 exports.qtarget:AddTargetModel({"v_ret_247shelves05", "v_ret_247shelves04", "v_ret_247shelves03"}, {
 options = {
 {
-event = "Steal",
+event = "xz-steal",
 icon = "fas fa-box-circle-check",
 label = "Steal",
 num = 1
