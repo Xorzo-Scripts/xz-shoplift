8b6ca5e46914f9732ba6537b01e2faf743ab78cc
xorzo <80540494+xorzo1@users.noreply.github.com>
2022-04-02 12:44:46 +0200

    Update items.sql

--- items.sql
+++ items.sql
@@ -1,2 +1,4 @@
 INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('snicker', 'snicker', 1, 0, 1);
-INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('Energy', 'Energy', 1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('cocacola', 'cocacola', 1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('sprite', 'sprite', 1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('burger', 'burger', 1, 0, 1);
