62803f5f23f8c68e1925ead00d51a2148de74d07
xorzo <80540494+xorzo1@users.noreply.github.com>
2022-04-02 12:42:29 +0200

    Create items.sql

---
+++ items.sql
@@ -0,0 +1,2 @@
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('snicker', 'snicker', 1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('Energy', 'Energy', 1, 0, 1);
